66d9e8f97239ec0d3a98dea061f02955fbc57cf2
isaac-hash <zikkychukwudulue@gmail.com>
2025-11-30 22:21:37 +0100

    modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Providers } from "@/providers"
 import { GoogleTagManager } from "@next/third-parties/google"
-
+import { Analytics } from "@vercel/analytics/next"
 import { Settings } from "@/types/settings"
 import { Footer } from "@/components/navigation/footer"
 import { Navbar } from "@/components/navigation/navbar"
@@ -54,15 +54,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      {Settings.gtmconnected && <GoogleTagManager gtmId={Settings.gtm} />}
-      <body className={`${inter.variable} font-regular`}>
-        <Providers>
-          <Navbar />
-          <main className="h-auto px-5 sm:px-8">{children}</main>
-          <Footer />
-        </Providers>
-      </body>
-    </html>
+    <>
+      <Analytics />
+      <html lang="en">
+        {Settings.gtmconnected && <GoogleTagManager gtmId={Settings.gtm} />}
+        <body className={`${inter.variable} font-regular`}>
+          <Providers>
+            <Navbar />
+            <main className="h-auto px-5 sm:px-8">{children}</main>
+            <Footer />
+          </Providers>
+        </body>
+      </html>
+    </>
   )
 }
